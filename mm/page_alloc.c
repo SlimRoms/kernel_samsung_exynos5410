73b72b26a00ecf1fa37897072c1cc9de45fbbdde
Alberto96 <albertoguanti@gmail.com>
2015-08-09 15:20:28 +0200

    mm: pass readahead info down to the i/o scheduler
    Some i/o schedulers (i.e. row-iosched, cfq-iosched) deploy an idling
    algorithm in order to be better synced with the readahead algorithm.
    Idling is a prediction algorithm for incoming read requests.
    
    In this patch we mark pages which are part of a readahead window, by
    setting a newly introduced flag. With this flag, the i/o scheduler can
    identify a request which is associated with a readahead page. This
    enables the i/o scheduler's idling mechanism to be en-sync with the
    readahead mechanism and, in turn, can increase read throughput.
    
    Change-Id: I0654f23315b6d19d71bcc9cc029c6b281a44b196
    Signed-off-by: Lee Susman <lsusman@codeaurora.org>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -5689,6 +5689,7 @@ static struct trace_print_flags pageflag_names[] = {
 	{1UL << PG_scfslower,		"scfslower"	},
 	{1UL << PG_nocache,		"nocache"	},
 #endif
+	{1UL << PG_readahead,           "PG_readahead"  },
 	{-1UL,				NULL		},
 };
 
